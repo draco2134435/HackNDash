b1b1f4fd3739fca02caf2c99adedc817f2b116ba
Austin Weng <shuubox7@gmail.com>
2016-09-24 15:41:56 -0400

    1st legit bug
    fuck pixar

--- HacknDash/Assets/Scripts/PlayerController.cs
+++ HacknDash/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2d myRigidbody; 
+
 	// Use this for initialization
 	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> (); 
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y); 
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce); 
+		}
 	
 	}
 }
